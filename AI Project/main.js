05465f8a96cc523d36770f5732f60dfd6f4daf0b
elx3020 <estebannl28@gmail.com>
2022-08-15 09:53:46 +0200

    road class and util class

--- AI Project/main.js
+++ AI Project/main.js
@@ -1,19 +1,19 @@
-const canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
+const ctx = canvas.getContext("2d");
 
-const ctx = canvas.getContext('2d');
-
-const car = new Car(100,100,30,50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLineCenter(1), 100, 30, 50);
 
 animate();
 
-
-function animate(){
-    car.update();
-    canvas.height = window.innerHeight;
-
-    car.draw(ctx);
-    requestAnimationFrame(animate);
-
+function animate() {
+  car.update();
+  canvas.height = window.innerHeight;
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+  road.draw(ctx);
+  car.draw(ctx);
+  requestAnimationFrame(animate);
 }
